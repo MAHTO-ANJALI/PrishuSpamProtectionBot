88f70841045648ec57215508d38023fe5fec095b
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-06-08 06:14:50 +0530

    Update info.py

--- spr/modules/info.py
+++ spr/modules/info.py
@@ -18,7 +18,7 @@ __HELP__ = """
 /info [CHAT_ID/Username|USER_ID/Username]
 
 or you can use inline mode >>
-@SpamProtectionRobot [CHAT_ID/Username|USER_ID/Username]
+@DangerousSpamProtectionBot [CHAT_ID/Username|USER_ID/Username]
 """
 
 
